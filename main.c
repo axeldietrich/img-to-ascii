f7c0b7049589e1210774e0ecdfd8127f56884611
Axel Dietrich <axeldietrich07@gmail.com>
2025-02-02 02:51:45 +0000

    Loops merged
    Replaced integer division for float multiplicatoin
    
    Implemented output buffer for efficient printing

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include "stb_image.h"
@@ -7,6 +9,7 @@
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		printf("Usage: %s image_file\n", argv[0]);
+		return 1;
 	}
 
 	char *restrict filename = argv[1];
@@ -40,24 +43,30 @@ int main(int argc, char *argv[]) {
 	int total_size = new_width * new_height * 3;
 	float c = 1.8f;
 	float b = 0.0f;
-	float scale_factor = 1.0f / 255.0f;
-	for (int i = 0, j = 0, y = 0; i < total_size; i += 3, j++) {
-		float val = (0.2126f * resized_img[i] + 0.7152f * resized_img[i + 1] + 0.0722f * resized_img[i + 2]) * scale_factor;
-		val = (val - 0.5f) * c + 0.5f + b;
-		if (val < 0.0f) val = 0.0f;
-		if (val > 1.0f) val = 1.0f;
-		resized_img[j] = (unsigned char)(val * 255);
+	float scale_factor = (ascii_length - 1) / 255.0f;
+
+	size_t output_size = (new_width + 1) * new_height;
+	char *restrict output = malloc((new_width + 1) * new_height);
+	if (output == NULL) {
+		printf("Memory allocation for output buffer failed\n");
+		return 1;
 	}
 
-	for (int y = 0; y < new_height; y++) {
-		for (int x = 0; x < new_width; x++) {
-			int pixel_value = resized_img[y * new_width + x];
-			int index = (ascii_length - 1) - (pixel_value * (ascii_length - 1) * scale_factor);
-			putchar(ascii_characters[index]);
+	for (int i = 0, j = 0, y = 0; i < total_size; i +=3, j++) {
+		float val = (0.2126f * resized_img[i] + 0.7152f * resized_img[i + 1] + 0.0722f * resized_img[i + 2]);
+		val = (val - 127.5f) * c + 127.5f + b;
+		int pixel_value = (unsigned char) fminf(fmaxf(val, 0.0f), 255.0f);
+		int index = (255 - pixel_value) * scale_factor;
+		output[y * (new_width + 1) + (j % new_width)] = ascii_characters[index];
+
+		if ((j + 1) % new_width == 0) {
+			output[y * (new_width + 1) + new_width] = '\n';
+			y++;
 		}
-		printf("\n");
 	}
+	write(STDOUT_FILENO, output, output_size);
 
 	free(resized_img);
+	free(output);
 	return 0;
 }
